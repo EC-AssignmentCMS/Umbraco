328093554337c3e888dcc6d60e27956eae6ca4c2
Joaaker <joakim.akerman@hotmail.com>
2025-10-24 13:56:32 +0200

    Footer done

--- CMS/wwwroot/js/site.js
+++ CMS/wwwroot/js/site.js
@@ -1,1 +1,12 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', () => {
+
+
+    //Footer Copyright Highlighting
+    document.querySelectorAll(".copyright-text").forEach(p => {
+        const words = p.innerHTML.trim().split(" ");
+        if (words.length > 1) {
+            const lastWord = words.pop();
+            p.innerHTML = `${words.join(" ")} <span class="highlight">${lastWord}</span>`;
+        }
+    });
+});
