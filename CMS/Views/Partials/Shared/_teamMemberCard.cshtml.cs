fa3fc70210e07a547e15215674fbf75feba6c0a4
Joaaker <joakim.akerman@hotmail.com>
2025-10-29 08:43:24 +0100

    Team section done

---
+++ CMS/Views/Partials/Shared/_teamMemberCard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CMS.Views.Partials.Shared
+{
+    public class _teamMemberCardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
